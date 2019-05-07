7af3a737c6b4934541b24600836276cee6c55a28
teruya <abas.trace.on@gmail.com>
2019-05-07 19:26:41 +0900

    unuse thread

--- src/udp_server.rs
+++ src/udp_server.rs
@@ -1,5 +1,5 @@
-use std::net::{ UdpSocket, SocketAddr };
-use std::{ str, thread };
+use std::net::UdpSocket;
+use std::str;
 
 pub fn serve(address: &str) -> Result<(), failure::Error> {
     let server_socket = UdpSocket::bind(address)?;
@@ -7,15 +7,7 @@ pub fn serve(address: &str) -> Result<(), failure::Error> {
         let mut buf = [0u8; 1024];
         let (size, src) = server_socket.recv_from(&mut buf)?;
         debug!("Handling data from {}", src);
-        let socket = server_socket.try_clone()?;
-        thread::spawn(move || {
-            handler(socket, src, &buf[..size]).unwrap_or_else(|error| error!("{:?}", error));
-        });
+        print!("{}", str::from_utf8(&buf[..size])?);
+        server_socket.send_to(&buf, src)?;
     }
-}
-
-fn handler(socket: UdpSocket, dest: SocketAddr, buf: &[u8]) -> Result<(), failure::Error> {
-    print!("{}", str::from_utf8(buf)?);
-    socket.send_to(&buf, dest)?;
-    Ok(())
 }
