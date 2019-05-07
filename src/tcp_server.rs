2860de015f583d99b52316673dbcd4e26cb17123
teruya <abas.trace.on@gmail.com>
2019-05-07 16:22:45 +0900

    tcp server

--- src/tcp_server.rs
+++ src/tcp_server.rs
@@ -2,10 +2,14 @@ use std::io::{Read, Write};
 use std::net::{TcpListener, TcpStream};
 use std::{str, thread};
 
+/**
+ * 指定のソケットアドレスで接続を待ち受ける
+ */
 pub fn serve(address: &str) -> Result<(), failure::Error> {
     let listener = TcpListener::bind(address)?;
     for stream in listener.incoming() {
         let stream = stream?;
+        // スレッドを立ち上げて接続に対処する。
         thread::spawn(move || {
             handler(stream).unwrap_or_else(|error| error!("{:?}", error));
         });
@@ -13,8 +17,10 @@ pub fn serve(address: &str) -> Result<(), failure::Error> {
     Ok(())
 }
 
+/**
+ * クライアントからの入力を待ち受け、受信したら同じものを返却する。
+ */
 fn handler(mut stream: TcpStream) -> Result<(), failure::Error> {
-    debug!("My addr is {}", stream.local_addr()?);
     debug!("Handling data from {}", stream.peer_addr()?);
     let mut buffer = [0u8; 1024];
     loop {
