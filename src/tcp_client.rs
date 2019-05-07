5a2d73246aede4c691613184b3ace9bd96374eff
teruya <abas.trace.on@gmail.com>
2019-05-07 16:22:39 +0900

    tcp client

--- src/tcp_client.rs
+++ src/tcp_client.rs
@@ -1,28 +1,22 @@
 use std::net::TcpStream;
-use std::{str, env};
-use std::time::Duration;
+use std::str;
 use std::io::{self, BufRead, BufReader, Write};
 
-fn main() {
-    let args: Vec<String> = env::args().collect();
-    if args.len() != 2 {
-        eprintln!("Please specify address and port. Usage: ./main [address]:[port]");
-        std::process::exit(1);
-    }
-    let remote = args[1].parse().expect("Usage: ./main [address]:[port]");
-
-    let mut stream = TcpStream::connect_timeout(&remote, Duration::from_secs(1)).expect("Could not connect.");
-    stream.set_read_timeout(Some(Duration::from_secs(2))).unwrap();
-    stream.set_write_timeout(Some(Duration::from_secs(2))).unwrap();
-
+/**
+ * 指定のIPアドレス、ポート番号にTCP接続する。
+ */
+pub fn connect(address: &str) -> Result<(), failure::Error>{
+    let mut stream = TcpStream::connect(address)?;
     loop {
+        // 入力データをソケットから送信。
         let mut input = String::new();
-        io::stdin().read_line(&mut input).unwrap();
-        stream.write(input.as_bytes()).expect("failed to write");
+        io::stdin().read_line(&mut input)?;
+        stream.write(input.as_bytes())?;
 
+        // ソケットから受信したデータを表示。
         let mut reader = BufReader::new(&stream);
         let mut buffer = Vec::new();
-        reader.read_until(b'\n', &mut buffer).expect("failed to read from the socket");
-        print!("{}", str::from_utf8(&buffer).expect("failed to convert to a String"));
+        reader.read_until(b'\n', &mut buffer)?;
+        print!("{}", str::from_utf8(&buffer)?);
     }
 }
